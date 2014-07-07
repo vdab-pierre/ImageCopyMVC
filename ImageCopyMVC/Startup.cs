820d7cec9aa3e0bc8f48978a1a3bb1eb9d4aaceb
vdab-pierre @desktop <pierre.colin@vdab.be>
2014-07-07 15:35:56 +0200

    init

---
+++ ImageCopyMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ImageCopyMVC.Startup))]
+namespace ImageCopyMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
